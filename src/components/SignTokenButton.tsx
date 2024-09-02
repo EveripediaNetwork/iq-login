f0b7e1dcac026f69dcf52e1b1edafb3a97a0932e
Srujan Gurram <srujangs8@gmail.com>
2024-09-02 10:59:32 +0530

    Add zustand as a new dependency and implement SignTokenButton component for user authentication

---
+++ src/components/SignTokenButton.tsx
@@ -0,0 +1,46 @@
+import { useAccount } from 'wagmi'
+import { useEffect } from 'react'
+import { useAuth } from '../lib/hooks/useAuth'
+
+const SignTokenButton = ({
+  handleRedirect,
+  handleTokenPass,
+}: {
+  handleRedirect: () => void
+  handleTokenPass?: (token: string) => Promise<void>
+}) => {
+  const { isConnected } = useAccount()
+  const { token, loading, reSignToken, error } = useAuth()
+
+  useEffect(() => {
+    const handleEffect = async () => {
+      if (token && isConnected) {
+        await handleTokenPass?.(token)
+        handleRedirect()
+      }
+    }
+    handleEffect()
+  }, [handleRedirect, handleTokenPass, isConnected, token])
+
+  return token && isConnected ? (
+    <p className="w-full max-w-md rounded-[4px] bg-white dark:bg-[#2D3748] border-[1px] dark:border-gray-600 px-[14px] py-[8px] text-center text-[16px] font-bold text-gray-800 dark:text-alpha-900">
+      {error
+        ? 'Something went wrong. Please try again.'
+        : loading
+          ? 'You should get a signature request from your wallet, Approve it to continue.'
+          : 'You are logged in 🚀'}
+    </p>
+  ) : (
+    <button
+      type="button"
+      disabled={!isConnected}
+      className="w-[146px] justify-center rounded-[4px] px-[14px] py-[8px] text-[16px] font-bold shadow-sm duration-200 hover:scale-105 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-500 dark:disabled:bg-gray-500 dark:disabled:text-gray-400"
+      onClick={() => reSignToken(true)}
+      aria-label="Sign Token"
+    >
+      Sign Token
+    </button>
+  )
+}
+
+export default SignTokenButton
