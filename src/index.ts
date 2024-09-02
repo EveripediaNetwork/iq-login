4273abf55c06bd53235acef3fd2a20a44538d226
Srujan Gurram <srujangs8@gmail.com>
2024-09-02 11:02:16 +0530

    Add new components, data, and integrations to the index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+// Components
 export * from './components/CustomAvatar'
 export * from './components/RainbowKitClientProvider'
+export * from './components/Login'
+export * from './components/SignTokenButton'
+
+// Data
 export * from './lib/data/iqTestnet'
 export * from './lib/data/rainbowKitTheme'
+
+// Integrations
 export * from './lib/integrations/web3-auth-connector'
