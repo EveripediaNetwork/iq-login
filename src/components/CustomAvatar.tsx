2495fcbce4d05223e81917aa3f9c2ea4245a1a3d
Adebesin Cell <adebesintolulope80@gmail.com>
2024-09-17 16:17:29 +0100

    Refactor Web3Auth integration and Avatar component

--- src/components/CustomAvatar.tsx
+++ src/components/CustomAvatar.tsx
@@ -1,21 +1,21 @@
-import BoringAvatar from 'boring-avatars';
-import type { AvatarComponent } from '@rainbow-me/rainbowkit';
+import BoringAvatar from "boring-avatars";
+import type { AvatarComponent } from "@rainbow-me/rainbowkit";
 
 export const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
-  return ensImage ? (
-    <img
-      src={ensImage}
-      alt='user'
-      width={size}
-      height={size}
-      style={{ borderRadius: 999 }}
-    />
-  ) : (
-    <BoringAvatar
-      size={size}
-      name={address}
-      variant='pixel'
-      colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
-    />
-  );
+	return ensImage ? (
+		<img
+			src={ensImage}
+			alt="user"
+			width={size}
+			height={size}
+			style={{ borderRadius: 999 }}
+		/>
+	) : (
+		<BoringAvatar
+			size={size}
+			name={address}
+			variant="pixel"
+			colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
+		/>
+	);
 };
--- src/components/Web3AuthProvider.tsx
+++ src/components/Web3AuthProvider.tsx
@@ -1,38 +1,38 @@
-import type { UserInfo } from '@web3auth/base';
-import type { Web3Auth } from '@web3auth/modal';
-import { createContext, useContext, useEffect, useState } from 'react';
+import type { UserInfo } from "@web3auth/base";
+import type { Web3Auth } from "@web3auth/modal";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const Web3AuthContext = createContext<{
-  web3Auth: Web3Auth | null;
-  user: Partial<UserInfo> | null;
+	web3Auth: Web3Auth | null;
+	user: Partial<UserInfo> | null;
 }>({ web3Auth: null, user: null });
 
 export function Web3AuthProvider({
-  children,
-  web3AuthInstance
+	children,
+	web3AuthInstance,
 }: {
-  children: React.ReactNode;
-  web3AuthInstance: Web3Auth;
+	children: React.ReactNode;
+	web3AuthInstance: Web3Auth;
 }) {
-  const [user, setUser] = useState<Partial<UserInfo> | null>(null);
+	const [user, setUser] = useState<Partial<UserInfo> | null>(null);
 
-  useEffect(() => {
-    web3AuthInstance.on('connected', async () => {
-      setUser(await web3AuthInstance.getUserInfo());
-    });
+	useEffect(() => {
+		web3AuthInstance.on("connected", async () => {
+			setUser(await web3AuthInstance.getUserInfo());
+		});
 
-    web3AuthInstance.on('disconnected', () => {
-      setUser(null);
-    });
-  }, [web3AuthInstance]);
+		web3AuthInstance.on("disconnected", () => {
+			setUser(null);
+		});
+	}, [web3AuthInstance]);
 
-  return (
-    <Web3AuthContext.Provider value={{ web3Auth: web3AuthInstance, user }}>
-      {children}
-    </Web3AuthContext.Provider>
-  );
+	return (
+		<Web3AuthContext.Provider value={{ web3Auth: web3AuthInstance, user }}>
+			{children}
+		</Web3AuthContext.Provider>
+	);
 }
 
 export function useWeb3Auth() {
-  return useContext(Web3AuthContext);
+	return useContext(Web3AuthContext);
 }
