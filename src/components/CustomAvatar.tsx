f4001de929bf3902a6584e7b88ccf76a7e67da7a
Adebesin Cell <adebesintolulope80@gmail.com>
2024-09-17 08:08:27 +0100

    Refactor code for consistency and readability

--- src/components/CustomAvatar.tsx
+++ src/components/CustomAvatar.tsx
@@ -1,23 +1,23 @@
-'use client';
+"use client";
 
-import BoringAvatar from 'boring-avatars';
-import type { AvatarComponent } from '@rainbow-me/rainbowkit';
+import BoringAvatar from "boring-avatars";
+import type { AvatarComponent } from "@rainbow-me/rainbowkit";
 
 export const CustomAvatar: AvatarComponent = ({ address, ensImage, size }) => {
-  return ensImage ? (
-    <img
-      src={ensImage}
-      alt='user'
-      width={size}
-      height={size}
-      style={{ borderRadius: 999 }}
-    />
-  ) : (
-    <BoringAvatar
-      size={size}
-      name={address}
-      variant='pixel'
-      colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
-    />
-  );
+	return ensImage ? (
+		<img
+			src={ensImage}
+			alt="user"
+			width={size}
+			height={size}
+			style={{ borderRadius: 999 }}
+		/>
+	) : (
+		<BoringAvatar
+			size={size}
+			name={address}
+			variant="pixel"
+			colors={["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]}
+		/>
+	);
 };
