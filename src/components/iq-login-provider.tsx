1b0574d30cacff7c97e3808a24028908e0bb31c7
Srujan Gurram <srujangs8@gmail.com>
2025-01-01 20:14:39 +0530

    Remove web3auth provider and commented out code

--- src/components/iq-login-provider.tsx
+++ src/components/iq-login-provider.tsx
@@ -1,15 +1,15 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
+// import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
 import type React from "react";
 import {
-	// cookieStorage,
+	cookieStorage,
 	cookieToInitialState,
-	// createStorage,
+	createStorage,
 	WagmiProvider,
 	createConfig,
 	http,
 } from "wagmi";
-import type { Chain } from "wagmi/chains";
+import { fraxtal, type Chain } from "wagmi/chains";
 import { createWeb3AuthInstance } from "../lib/integrations/web3-auth-connector";
 import { Web3AuthProvider } from "./web3-auth-provider";
 import { injected, metaMask } from "wagmi/connectors";
@@ -28,40 +28,53 @@ export const ProjectContext = createContext<string>("");
 
 const queryClient = new QueryClient();
 
+const config = createConfig({
+	chains: [fraxtal],
+	transports: {
+		[fraxtal.id]: http(),
+	},
+
+	connectors: [injected(), metaMask()],
+	storage: createStorage({
+		storage: cookieStorage,
+	}),
+	ssr: true,
+	multiInjectedProviderDiscovery: false,
+});
+
 export function IqLoginProvider({
 	children,
-	// cookie,
+	cookie,
 	chain,
 	web3AuthProjectId,
 	projectName,
 }: IqLoginProviderProps) {
 	const web3AuthInstance = createWeb3AuthInstance(chain, web3AuthProjectId);
 
-	const config = createConfig({
-		chains: [chain],
-		transports: {
-			[chain.id]: http(),
-		},
+	// const config = createConfig({
+	// 	chains: [chain],
+	// 	transports: {
+	// 		[chain.id]: http(),
+	// 	},
 
-		connectors: [
-			Web3AuthConnector({ web3AuthInstance }),
-			injected(),
-			metaMask(),
-		],
-		// storage: createStorage({
-		// 	key: `wagmi-store-${projectName}`,
-		// 	storage: cookieStorage,
-		// }),
-		// ssr: true,
-		multiInjectedProviderDiscovery: false,
-	});
+	// 	connectors: [
+	// 		Web3AuthConnector({ web3AuthInstance }),
+	// 		injected(),
+	// 		metaMask(),
+	// 	],
+	// 	storage: createStorage({
+	// 		key: `wagmi-store-${projectName}`,
+	// 		storage: cookieStorage,
+	// 	}),
+	// 	ssr: true,
+	// 	multiInjectedProviderDiscovery: false,
+	// });
 
-	// const initialStates = cookieToInitialState(config, cookie);
+	const initialStates = cookieToInitialState(config, cookie);
 
 	return (
 		<ProjectContext.Provider value={projectName}>
-			{/* <WagmiProvider config={config} initialState={initialStates}> */}
-			<WagmiProvider config={config}>
+			<WagmiProvider config={config} initialState={initialStates}>
 				<QueryClientProvider client={queryClient}>
 					<Web3AuthProvider web3AuthInstance={web3AuthInstance}>
 						{children}
