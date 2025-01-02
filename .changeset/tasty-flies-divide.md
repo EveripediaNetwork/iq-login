eba3637810483fefffe7ca351e1137eb2a031d40
Srujan Gurram <srujangs8@gmail.com>
2025-01-02 12:12:51 +0530

    Refactor web3AuthInstance to use wagmi config

---
+++ .changeset/tasty-flies-divide.md
@@ -0,0 +1,5 @@
+---
+"@everipedia/iq-login": patch
+---
+
+query client and wagmi config refactoring
--- src/lib/integrations/wagmi.config.ts
+++ src/lib/integrations/wagmi.config.ts
@@ -1,7 +1,16 @@
 import * as Web3AuthBase from "@web3auth/base";
 import * as Web3AuthEthereumProvider from "@web3auth/ethereum-provider";
 import * as Web3AuthModal from "@web3auth/modal";
+import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
 import { type Chain, fraxtal, mainnet, polygon } from "viem/chains";
+import {
+	type Config,
+	cookieStorage,
+	createConfig,
+	createStorage,
+	http,
+} from "wagmi";
+import { injected, metaMask } from "wagmi/connectors";
 import { iqTestnet } from "../data/iq-testnet";
 
 const chainId = Number(process.env.NEXT_PUBLIC_CHAIN_ID) as number;
@@ -46,13 +55,23 @@ const chainConfig = {
 	blockExplorerUrl: chain.blockExplorers?.default.url[0] as string,
 };
 
-const privateKeyProvider =
-	new Web3AuthEthereumProvider.EthereumPrivateKeyProvider({
-		config: { chainConfig },
-	});
-
 export const web3AuthInstance = new Web3AuthModal.Web3Auth({
 	clientId: web3AuthClientId,
-	privateKeyProvider,
+	privateKeyProvider: new Web3AuthEthereumProvider.EthereumPrivateKeyProvider({
+		config: { chainConfig },
+	}),
 	web3AuthNetwork: Web3AuthBase.WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+});
+
+export const wagmiConfig: Config = createConfig({
+	chains: [chain],
+	transports: {
+		[chain.id]: http(),
+	},
+	connectors: [Web3AuthConnector({ web3AuthInstance }), injected(), metaMask()],
+	storage: createStorage({
+		storage: cookieStorage,
+	}),
+	ssr: true,
+	multiInjectedProviderDiscovery: false,
 });
