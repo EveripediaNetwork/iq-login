552955170bbbf7186028d9a22496e0db319f86b4
Srujan Gurram <srujangs8@gmail.com>
2025-02-24 18:22:56 +0530

    Update success message display logic in login element component

--- src/components/login-element.tsx
+++ src/components/login-element.tsx
@@ -150,7 +150,14 @@ const ConnectedWalletView = ({
 				<p className="font-mono text-xs break-all">{address}</p>
 			</div>
 
-			{!disableAuth && (
+			{disableAuth ? (
+				<div className="w-full grid place-items-center">
+					<div className="flex items-center gap-2 text-success py-1.5 text-green-500">
+						<CheckCircle className="h-4 w-4" />
+						<p className="font-medium text-sm">Successfully signed in!</p>
+					</div>
+				</div>
+			) : (
 				<VerificationSection
 					signTokenText={signTokenText}
 					handleRedirect={handleRedirect ?? (() => {})}
